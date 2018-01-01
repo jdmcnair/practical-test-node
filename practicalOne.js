272ef0e2696772cb59f990f1679104792583fbe0
Jason McNair <jmcnair@surgeforward.com>
2018-01-01 16:32:08 -0600

    adding tests for practical one

---
+++ practicalOne.js
@@ -0,0 +1,19 @@
+var _ = require('lodash');
+
+function reverseWordsInString(s) {
+    throw new Error('Not implemented');
+}
+
+function isPalindrome(palindrome) {
+    throw new Error('Not implemented');
+}
+
+function doCalculation(expression) {
+    throw new Error('Not implemented');
+}
+
+module.exports = {
+    reverseWordsInString: reverseWordsInString,
+    isPalindrome: isPalindrome,
+    doCalculation: doCalculation
+};
---
+++ test/practicalOneTest.js
@@ -0,0 +1,34 @@
+var expect = require('chai').expect;
+var practicalOne = require('../practicalOne');
+
+describe('reverseWordsInString()', function () {
+  it('should reverse words in string', function () {
+    // ASSERTIONS
+    expect('phil is name my Hello').to.be.equal(practicalOne.reverseWordsInString('Hello my name is phil'));
+    expect('shins my kicking stop George,').to.be.equal(practicalOne.reverseWordsInString('George, stop kicking my shins'));
+    expect('Strings-with-hyphens-are-still-one-word').to.be.equal(practicalOne.reverseWordsInString('Strings-with-hyphens-are-still-one-word'));
+    expect('?  spaces  Two').to.be.equal(practicalOne.reverseWordsInString('Two  spaces  ?'));
+  });
+});
+
+describe('isPalindrome()', function () {
+  it('should determine if string is a palindrome', function () {
+    // ASSERTIONS
+    expect(true).to.be.equal(practicalOne.isPalindrome('racecar'));
+    expect(true).to.be.equal(practicalOne.isPalindrome('Hannah'));
+    expect(true).to.be.equal(practicalOne.isPalindrome('Able was I ere I saw Elba'));
+    expect(false).to.be.equal(practicalOne.isPalindrome('Kevin likes to climb rocks'));
+    expect(false).to.be.equal(practicalOne.isPalindrome('Hannahash'));
+  });
+});
+
+describe('doCalculation()', function () {
+  it('should perform calculation in passed in expression', function () {
+    // ASSERTIONS
+    expect(2).to.be.equal(practicalOne.doCalculation('1+1'));
+    expect(4).to.be.equal(practicalOne.doCalculation('1+1+1+1'));
+    expect(3).to.be.equal(practicalOne.doCalculation('1+3-1'));
+    expect(2).to.be.equal(practicalOne.doCalculation('1-1+2'));
+    expect(7).to.be.equal(practicalOne.doCalculation('1-1+2+3+4+5-3-5-2+2-2+3'));
+  });
+});
